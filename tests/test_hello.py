24ebdb8af3903c52de199913a9942295892a4726
Patryk Czekaj <72671804+patryksc3@users.noreply.github.com>
2025-12-02 11:33:26 +0100

    test test

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,4 +5,4 @@ def test_hello():
     client = TestClient(app)
     resp = client.get('/hello')
     assert resp.status_code == 200
-    assert resp.json() == {"message": "Hello world"}
+    assert resp.json() == {"message": "Hello world!"}
